b14c8eeae19526509b5f143c3acb5dd6e2841974
Tom Tubbs <tomtubbs@mac.com>
2023-07-19 23:17:55 +0100

    CoreDataProject uses same project to look at multiple things. Deleting the FetchRequest variations from ContentView

--- CoreDataProject/ContentView.swift
+++ CoreDataProject/ContentView.swift
@@ -5,38 +5,50 @@
 //  Created by Tom on 19/07/2023.
 //
 
+import CoreData
 import SwiftUI
 
-
-struct Student: Hashable {
-	let name: String
-}
-
 struct ContentView: View {
-	
 	@Environment(\.managedObjectContext) var moc
 	
-	@FetchRequest(sortDescriptors: []) var wizards: FetchedResults<Wizard>
+//	@FetchRequest(sortDescriptors: [], predicate: nil) var ships: FetchedResults<Ship>
+//	@FetchRequest(sortDescriptors: [], predicate: NSPredicate(format: "universe == 'Star Wars'")) var ships: FetchedResults<Ship>
+//	@FetchRequest(sortDescriptors: [], predicate: NSPredicate(format: "universe == %@", "Star Wars")) var ships: FetchedResults<Ship>
 	
-	let students = [Student(name: "Harry Potter"), Student(name: "Hermione Granger")]
+//	@FetchRequest(sortDescriptors: [], predicate: NSPredicate(format: "name < %@", "F")) var ships: FetchedResults<Ship>
+ 
+	
+//	@FetchRequest(sortDescriptors: [], predicate: NSPredicate(format: "name BEGINSWITH %@", "E")) var ships: FetchedResults<Ship>
+	
+//	@FetchRequest(sortDescriptors: [], predicate: NSPredicate(format: "name BEGINSWITH[c] %@", "e")) var ships: FetchedResults<Ship>
+	
+ 	@FetchRequest(sortDescriptors: [], predicate: NSPredicate(format: "NOT name BEGINSWITH[c] %@", "e")) var ships: FetchedResults<Ship>
+	
 	
 	var body: some View {
 		VStack {
-			List(wizards, id: \.self) { wizard in
-				Text(wizard.name ?? "Unknown")
+			List(ships, id: \.self) { ship in
+				Text(ship.name ?? "Unknown name")
 			}
-			
-			Button("Add") {
-				let wizard = Wizard(context: moc)
-				wizard.name = "Harry Potter"
-			}
-			
-			Button("Save") {
-				do {
-					try moc.save()
-				} catch {
-					print(error.localizedDescription)
-				}
+
+			Button("Add Examples") {
+				let ship1 = Ship(context: moc)
+				ship1.name = "Enterprise"
+				ship1.universe = "Star Trek"
+
+				let ship2 = Ship(context: moc)
+				ship2.name = "Defiant"
+				ship2.universe = "Star Trek"
+
+				let ship3 = Ship(context: moc)
+				ship3.name = "Millennium Falcon"
+				ship3.universe = "Star Wars"
+
+				let ship4 = Ship(context: moc)
+				ship4.name = "Executor"
+				ship4.universe = "Star Wars"
+
+				try? moc.save()
 			}
 		}
 	}
---
+++ CoreDataProject/Singer+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  Singer+CoreDataClass.swift
+//  CoreDataProject
+//
+//  Created by Tom on 19/07/2023.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(Singer)
+public class Singer: NSManagedObject {
+
+}
